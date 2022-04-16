74f97a21c4f8bad9746b7e7a31b6e0742c65c634
lursmana <giglagigla2010@gmail.com>
2022-04-16 21:26:32 +0400

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+
+function App() {
+  return (
+    <div>app</div>
+  )
+}
+
+export default App
