60e1f02167215b66c5cd8c5eae34e3a588a78834
lursmana <88488438+lursmana@users.noreply.github.com>
2022-05-05 21:29:20 +0400

    news done

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../servises/cryptoApi";
+import { cryptoNewsApi } from "../servises/cryptoNewsApi";
 
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
     }
 })
--- src/components/Cryptocurrensies.jsx
+++ src/components/Cryptocurrensies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
@@ -8,11 +8,30 @@ const Cryptocurrensies = ({simplified}) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    
+    const filteredData = cryptosList?.data.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    setCryptos(filteredData)
+
+  }, [searchTerm, cryptosList])
+  
 
   if(isFetching) return 'Loading...'
 
+  
+
   return (
     <>
+    {!simplified && (
+    <div className='search-crypto'>
+      <input placeholder='Search Cryptocurrencie' onChange={(e) => setSearchTerm(e.target.value)} />
+    </div>
+    )}
+
+
     <Row gutter={[32, 32]} className='crypto-card-container'>
       {cryptos?.map((currency) => (
         <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
